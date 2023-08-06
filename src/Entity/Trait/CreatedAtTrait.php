33419c8a829a12a5437e43ff8be724f301e356cc
Anthony <semprini.anthony@gmail.com>
2023-08-06 16:57:38 +0200

    sortie les infos created at des entity pour les mettre dans le trait

---
+++ src/Entity/Trait/CreatedAtTrait.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace App\Entity\Trait;
+
+Use Doctrine\ORM\Mapping as ORM;
+
+trait CreatedAtTrait 
+{
+    #[ORM\Column(type: 'datetime_immutable', options: [
+        'default' => 'CURRENT_TIMESTAMP'])]
+    private $created_at;
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+}
