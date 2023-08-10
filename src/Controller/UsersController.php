2e8b268ce1b8b35ff18791818055a3723f129420
Anthony <semprini.anthony@gmail.com>
2023-08-10 23:38:36 +0300

    debug de controllerusers

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -1,12 +1,11 @@
 <?php
 
-namespace App\Controller\admin;
+namespace App\Controller;
 
 
-use Symfony\Component\HttpFoundation\Response;
-
-use Symfony\component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/admin/users', name: 'app_admin_users')]
 class UsersController extends AbstractController
@@ -14,9 +13,8 @@ class UsersController extends AbstractController
     #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('admin/users/index.html.twig');
+        return $this->render('admin/users/index.html.twig', [
+            'controller_name' => 'UsersController',
+        ]);
     }
-
-
-
 }
